647bb970b2e2cc2eec8f38cd5e9991128cd253f0
Pedro <pedro_canassa@hotmail.com>
2022-03-04 15:23:13 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+
+import { Header } from '@components/Header'
+import { SubscribeButton } from '@components/SubscribeButton'
+
+import styles from '@styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <Header />
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  )
+}
